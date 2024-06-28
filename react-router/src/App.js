cb8e124d03ad2b4f630c83dd7de5280b6232c093
Bharath2228 <pbharath2210@gmail.com>
2024-06-28 21:32:17 +0530

    react-router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Header } from "./components/Header"
+import { Home } from "./components/Home"
+import { ProductList } from "./components/ProductList"
+import { ProductDetail } from "./components/ProductDetail"
+import { Contact } from "./components/Contact"
+import { Footer } from "./components/Footer"
+
+import './App.css'
 
 export const App = () => {
   return (
-    <div>App</div>
+
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="products" element={<ProductList />}></Route>
+          <Route path="products/1001" element={<ProductDetail />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+        </Routes>
+      </main>
+      
+      <Footer />
+    </div>
+    
   )
 }
--- react-router/src/components/Header.js
+++ react-router/src/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import Logo from "../assets/logo.png"
+import { Link, NavLink } from "react-router-dom"
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="Logo"/>
+        <span>React Router</span>
+      </Link>
+      <nav className="navigation">
+        <NavLink to="/" className="link">Home</NavLink>
+        <NavLink to="/products" className="link">Products</NavLink>
+        <NavLink to="/contact" className="link">Contact</NavLink>
+      </nav>
+    </header>
   )
 }
